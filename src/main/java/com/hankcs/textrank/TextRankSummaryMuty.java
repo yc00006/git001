62f65868f3ae9fc0b8204d0f51988d1e0564b2aa
yc00006 <2804354903@qq.com>
2024-01-30 11:08:10 +0800

    练习提交和撤回

--- src/main/java/com/hankcs/textrank/TextRankSummaryMuty.java
+++ src/main/java/com/hankcs/textrank/TextRankSummaryMuty.java
@@ -41,8 +41,8 @@ public class TextRankSummaryMuty {
                 "一，有限的确定性算法，这类算法在有限的一段时间内终止。他们可能要花很长时间来执行指定的任务，但仍将在一定的时间内终止。这类算法得出的结果常取决于输入值。\n" +
                 "二，有限的非确定算法，这类算法在有限的时间内终止。然而，对于一个（或一些）给定的数值，算法的结果并不是唯一的或确定的。\n" +
                 "三，无限的算法，是那些由于没有定义终止定义条件，或定义的条件无法由输入的数据满足而不终止运行的算法。通常，无限算法的产生是由于未能确定的定义终止条件。";
-        //String fileName = "src/test/doc/03.txt";
-        //String rf = ReadFromFile.file2String(fileName,"utf8");
+        String fileName = "src/test/doc/01.txt";
+        String rf = ReadFromFile.file2String(fileName,"utf8");
         //System.out.println(TextRankSummary.getTopSentenceList(rf, 3));
         //System.out.println(TextRankSummary.getTopSentenceListEng(rf, 3));
 
@@ -54,8 +54,12 @@ public class TextRankSummaryMuty {
             size = args[1]!=null&&!"".equals(args[1])? Integer.parseInt(args[1]):3;
             lang = args[2];
         }
+
+        long begin = System.currentTimeMillis();
         System.out.println(TextRankSummaryMuty.getTopSentenceListMulty(doc,size,lang));
         //System.out.println("--------------------------------------");
-        //System.out.println(new TextRankKeyword().getKeyword("", rf));
+        System.out.println(new TextRankKeyword().getKeyword("", doc));
+        long time = System.currentTimeMillis() - begin;
+        System.out.println("time = " + time);
     }
 }
